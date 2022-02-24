d2d83bb9a4c13fb1fad9edb15f6ca765519e4b23
Nathan Walston <nathanielwalston@gmail.com>
2022-02-24 09:24:43 -0800

    Add objects to the UI

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-import { Scene } from "three";
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from "three";
 
 const scene = new Scene();
+const camera = new PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new WebGLRenderer();
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+
+scene.add(cube);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.z = 5;
+
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+
+setInterval(function () {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+}, 8)
